7ae6010d472082523da17a1007daf295617c26bb
abdullah-alkhareji <outsiderandco@gmail.com>
2022-01-23 18:22:09 +0300

    added react-router-dom

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,33 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 function Nav() {
-  return (
-    <nav
-      class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top"
-      id="mainNav"
-    >
-      <div class="container">
-        <a class="navbar-brand" href="#">
-          Hike
-        </a>
+	return (
+		<nav
+			className='navbar navbar-expand-lg bg-secondary text-uppercase fixed-top'
+			id='mainNav'>
+			<div className='container'>
+				<Link className='navbar-brand' to='/'>
+					Hike
+				</Link>
 
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item mx-0 mx-lg-1">
-              <a class="nav-link py-3 px-0 px-lg-3 rounded" href="#">
-                Home
-              </a>
-            </li>
-            <li class="nav-item mx-0 mx-lg-1">
-              <a class="nav-link py-3 px-0 px-lg-3 rounded" href="#">
-                Trips
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+				<div className='collapse navbar-collapse' id='navbarResponsive'>
+					<ul className='navbar-nav ms-auto'>
+						<li className='nav-item mx-0 mx-lg-1'>
+							<NavLink className='nav-link py-3 px-0 px-lg-3 rounded' to='/'>
+								Home
+							</NavLink>
+						</li>
+						<li className='nav-item mx-0 mx-lg-1'>
+							<NavLink
+								className='nav-link py-3 px-0 px-lg-3 rounded'
+								to='/tripslist'>
+								Trips
+							</NavLink>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
 }
 
 export default Nav;
--- src/components/TripDetail.js
+++ src/components/TripDetail.js
@@ -1,44 +1,48 @@
-import React from 'react';
-import tripsData from '../tripsData';
+import React from "react";
+import tripsData from "../tripsData";
 
 function TripDetail() {
-  const trip = tripsData[0];
-  return (
-    <div class="modal-dialog modal-xl">
-      <div class="modal-content">
-        <div class="modal-body text-center pb-5">
-          <div class="container">
-            <div class="row justify-content-center">
-              <div class="col-lg-8">
-                <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">
-                  {trip.name}
-                </h2>
-                <div class="divider-custom">
-                  <div class="divider-custom-line"></div>
-                  <div class="divider-custom-icon">
-                    <i class="fas fa-star"></i>
-                  </div>
-                  <div class="divider-custom-line"></div>
-                </div>
-                <img class="img-fluid rounded mb-5" src={trip.img} alt="..." />
-                <p class="mb-4">
-                  City : {trip.city}
-                  <br />
-                  Length : {trip.length1}Km
-                  <br />
-                  Rating : {trip.rating}
-                  <br />
-                  Difficulty : {trip.difficulty}
-                  <br />
-                  Details : {trip.details}
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const trip = tripsData[0];
+	return (
+		<div className='page-section modal-dialog modal-xl mt-5'>
+			<div className='modal-content'>
+				<div className='modal-body text-center pb-5'>
+					<div className='container'>
+						<div className='row justify-content-center'>
+							<div className='col-lg-8'>
+								<h2 className='portfolio-modal-title text-secondary text-uppercase mb-0'>
+									{trip.name}
+								</h2>
+								<div className='divider-custom'>
+									<div className='divider-custom-line'></div>
+									<div className='divider-custom-icon'>
+										<i className='fas fa-star'></i>
+									</div>
+									<div className='divider-custom-line'></div>
+								</div>
+								<img
+									className='img-fluid rounded mb-5'
+									src={trip.img}
+									alt='...'
+								/>
+								<p className='mb-4'>
+									City : {trip.city}
+									<br />
+									Length : {trip.length1}Km
+									<br />
+									Rating : {trip.rating}
+									<br />
+									Difficulty : {trip.difficulty}
+									<br />
+									Details : {trip.details}
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default TripDetail;
